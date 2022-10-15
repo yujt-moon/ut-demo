6e6f5580d6847a9b704f744fb1c6b674d812a384
yujt-moon <1291932524@qq.com>
2022-10-15 10:51:07 +0800

    代码初始化

---
+++ spring-ut/springboot-ut/src/main/java/com/huawei/springbootut/SpringbootUtApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.springbootut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUtApplication.class, args);
+    }
+
+}
---
+++ spring-ut/springboot-ut/src/test/java/com/huawei/springbootut/SpringbootUtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huawei.springbootut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
